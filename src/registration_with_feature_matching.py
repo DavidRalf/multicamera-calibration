b74ca193c5da42fe8b2234863e835783f5b7a1d7
David <David.Ralf@haw-hamburg.de>
2024-08-22 02:10:13 +0200

    some changes

--- src/registration_with_feature_matching.py
+++ src/registration_with_feature_matching.py
@@ -1,117 +1,112 @@
-#umwandlung des Jupyter Notebooks Alignment V2 in ein Python skript
-import os, glob
-import sys
+# umwandlung des Jupyter Notebooks Alignment V2 in ein Python skript
 import argparse
-import imageprocessing.micasense.capture as capture
+import os
+import sys
 from pathlib import Path
-import utils as utils
-import matplotlib.pyplot as plt
-from skimage.transform import ProjectiveTransform
+
+import numpy
 import numpy as np
-import time
+import skimage
+from skimage.transform import ProjectiveTransform
 
-# Define the argument parser
-parser = argparse.ArgumentParser(description='Process multispectral images.')
+import imageprocessing.micasense.capture as capture
+import utils as utils
+from src.micasense.registered_micasense import RegisteredMicasense
 
-# Add positional arguments
-parser.add_argument('image_path', type=str, help='Path to the directory containing images')
-# Make image_number optional
-parser.add_argument('image_number', type=str, nargs='?', default=None,
-                    help='Image number with leading zeros (e.g., 0059)')
-parser.add_argument('save_warp_matrices', type=str, help='True or False for saving warp matrices')
 
-# Parse the arguments
-args = parser.parse_args()
+def register_image_with_feature_matching(thecapture, batch, version, save_warp_matrices, regenerate=True):
+    cam_serial = thecapture.camera_serial
+    warp_matrices_filename = 'output/warp/' + cam_serial + "_warp_matrices_SIFT.npy"
+    if Path(warp_matrices_filename).is_file() and not regenerate:
+        print("Found existing warp matrices for camera", cam_serial)
+        load_warp_matrices = np.load(warp_matrices_filename, allow_pickle=True)
+        loaded_warp_matrices = []
+        for matrix in load_warp_matrices:
+            transform = ProjectiveTransform(matrix=matrix.astype('float64'))
+            loaded_warp_matrices.append(transform)
+        print("Warp matrices successfully loaded.")
 
-# Access the arguments
-image_path = Path(args.image_path)
-image_number = args.image_number
-save_warp_matrices = args.save_warp_matrices.lower() in ['true', '1', 't', 'y', 'yes']
-if image_number is None:
-    image_number = "*"
-    print("test")
-# Collect image names that match the pattern
-image_names = list(image_path.glob(f'IMG_{image_number}_*.tif'))
-image_names = [x.as_posix() for x in image_names]
+        warp_matrices_SIFT = loaded_warp_matrices
+    else:
+        print("No existing warp matrices found. Create them later.")
+        warp_matrices_SIFT = False
 
-# Check if we have enough pictures
-if len(image_names) < 6:
-    print(f"Warning: Expected at least 6 images, but found {len(image_names)}. Please check the image files.")
-    sys.exit()  # Use sys.exit() to terminate the script if there are not enough images
-batch = image_names[0:0 + 6]  # Take 6 images at a time
-batch.sort()
-thecapture = capture.Capture.from_filelist(batch)
-cam_serial = thecapture.camera_serial
-warp_matrices_filename = cam_serial + "_warp_matrices_SIFT.npy"
-if Path('./' + warp_matrices_filename).is_file():
-    print("Found existing warp matrices for camera", cam_serial)
-    load_warp_matrices = np.load(warp_matrices_filename, allow_pickle=True)
-    loaded_warp_matrices = []
-    for matrix in load_warp_matrices:
-        transform = ProjectiveTransform(matrix=matrix.astype('float64'))
-        loaded_warp_matrices.append(transform)
-    print("Warp matrices successfully loaded.")
-
-    warp_matrices_SIFT = loaded_warp_matrices
-else:
-    print("No existing warp matrices found. Create them later in the notebook.")
-    warp_matrices_SIFT = False
-    warp_matrices = False
-# Process images in batches of 6
-for i in range(0, len(image_names), 6):
-    batch = image_names[i:i + 6]  # Take 6 images at a time
-    batch.sort()
-    file_name = utils.extract_image_name(batch[0])
+    file_names = utils.extract_all_image_names(batch)
     thecapture = capture.Capture.from_filelist(batch)
 
     if thecapture.dls_present():
         img_type = 'reflectance'
         irradiance_list = thecapture.dls_irradiance() + [0]
-        thecapture.plot_undistorted_reflectance(thecapture.dls_irradiance())
     else:
         img_type = "radiance"
-        thecapture.plot_undistorted_radiance()
         irradiance_list = None
 
-    utils.make_rgb_composite_from_original(thecapture, irradiance_list,"output/rgb_composite_original.png")
-    st = time.time()
     if not warp_matrices_SIFT:
         print("Generating new warp matrices...")
         warp_matrices_SIFT = thecapture.SIFT_align_capture(min_matches=10)
 
-    sharpened_stack, upsampled = thecapture.radiometric_pan_sharpened_aligned_capture(warp_matrices=warp_matrices_SIFT,
-                                                                                      irradiance_list=irradiance_list,
-                                                                                      img_type=img_type)
+    sharpened_stack, upsampled = thecapture.radiometric_pan_sharpened_aligned_capture(
+        warp_matrices=warp_matrices_SIFT,
+        irradiance_list=irradiance_list,
+        img_type=img_type)
 
-    # we can also use the Rig Relatives from the image metadata to do a quick, rudimentary alignment
-    #     warp_matrices0=thecapture.get_warp_matrices(ref_index=5)
-    #     sharpened_stack,upsampled = radiometric_pan_sharpen(thecapture,warp_matrices=warp_matrices0)
+    if save_warp_matrices:
+        working_wm = warp_matrices_SIFT
+        temp_matrices = []
+        for x in working_wm:
+            if isinstance(x, numpy.ndarray):
+                temp_matrices.append(x)
+            if isinstance(x, skimage.transform._geometric.ProjectiveTransform):
+                temp_matrices.append(x.params)
+        # Create the directory if it does not exist
+        os.makedirs("output/warp", exist_ok=True)
+        np.save(warp_matrices_filename, np.array(temp_matrices, dtype=object), allow_pickle=True)
+        print("Saved to", Path(warp_matrices_filename).resolve())
 
-    print("Pansharpened shape:", sharpened_stack.shape)
-    print("Upsampled shape:", upsampled.shape)
-    # re-assign to im_aligned to match rest of code
-    im_aligned = upsampled
-    et = time.time()
-    elapsed_time = et - st
-    print('Alignment and pan-sharpening time:', int(elapsed_time), 'seconds')
+    if version == "stack":
+        registered_images = RegisteredMicasense(sharpened_stack, file_names)
 
-    # set output name to unique capture ID, e.g. FWoNSvgDNBX63Xv378qs
-    outputName = "output/" + file_name
+    else:
+        registered_images = RegisteredMicasense(upsampled, file_names)
 
-    st = time.time()
-    print("im_aligned")
-    print(im_aligned)
-    # in this example, we can export both a pan-sharpened stack and an upsampled stack
-    # so you can compare them in GIS. In practice, you would typically only output the pansharpened stack
-    thecapture.save_capture_as_stack(outputName + "-pansharpened.tif", sort_by_wavelength=False, pansharpen=True)
-    thecapture.save_capture_as_stack(outputName + "-upsampled.tif", sort_by_wavelength=False, pansharpen=False)
-    # Save each band to its own TIFF file
-    #thecapture.save_each_band_as_tif(outputName+)
+    return registered_images
 
-    et = time.time()
-    elapsed_time = et - st
-    print("Time to save stacks:", int(elapsed_time), "seconds.")
-    #print(thecapture._Capture__aligned_radiometric_pan_sharpened_capture[0])
-    # print(thecapture._Capture__aligned_radiometric_pan_sharpened_capture[0][0])
-    thecapture = None
-    break
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Registration of the micasense images with feature matching.')
+    parser.add_argument('image_path', type=str, help='Path to the directory containing images')
+    parser.add_argument('save_warp_matrices', type=str, help='True or False for saving warp matrices')
+    parser.add_argument('version', type=str, help='Stack or Upsampled')
+    parser.add_argument('output', type=str, nargs='?', default="output/feature_matching", help='Path to save images')
+    parser.add_argument('image_number', type=str, nargs='?', default=None,
+                        help='Image number with leading zeros (e.g., 0059)')
+
+    args = parser.parse_args()
+
+    # Access the arguments
+    image_path = Path(args.image_path)
+    image_number = args.image_number
+    version = args.version.lower()
+    output = args.output
+    save_warp_matrices = args.save_warp_matrices.lower() in ['true', '1', 't', 'y', 'yes']
+    if image_number is None:
+        image_number = "*"
+
+    # Collect image names that match the pattern
+    image_names = list(image_path.glob(f'IMG_{image_number}_*.tif'))
+    image_names = sorted([x.as_posix() for x in image_names])
+
+    # Check if we have enough pictures
+    if len(image_names) < 6:
+        print(f"Warning: Expected at least 6 images, but found {len(image_names)}. Please check the image files.")
+        sys.exit()
+
+    for i in range(0, len(image_names), 6):
+        batch = image_names[i:i + 6]
+        if len(batch) < 6:
+            print(f"Skipping incomplete batch: {batch}. Not enough images.")
+            continue
+        print(f"Processing batch: {batch}")
+        thecapture = capture.Capture.from_filelist(batch)
+        registered_images = register_image_with_feature_matching(thecapture, batch, version, save_warp_matrices)
+        registered_images.save_images(output)
