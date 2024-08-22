78dd8a57a2c6a299eb9fab78ca100b375c452234
David <David.Ralf@haw-hamburg.de>
2024-08-22 03:51:05 +0200

    some changes and now uses RegisteredMicasense class to handle the registered images

--- src/registration_with_depth_matching.py
+++ src/registration_with_depth_matching.py
@@ -1,24 +1,20 @@
 import argparse
+import sys
 from pathlib import Path
-import imageprocessing.micasense.capture as capture
+
 import cv2
 import numpy as np
-import utils as utils
-import matplotlib.pyplot as plt
 
-names = {"Blue",
-         "Green",
-         "Red",
-         "NIR",
-         "Red edge",
-         "Panchro"}
+import imageprocessing.micasense.capture as capture
+import utils as utils
+from src.micasense.registered_micasense import RegisteredMicasense
 
 
 def pixel_to_3d(cam_positions, depth, intrinsic):
     fx, fy = intrinsic[0, 0], intrinsic[1, 1]
     cx, cy = intrinsic[0, 2], intrinsic[1, 2]
 
-   # = np.ones_like(depth)
+    # = np.ones_like(depth)
     Z = depth
     x = cam_positions[..., 0]
     y = cam_positions[..., 1]
@@ -35,25 +31,23 @@ def project_3d_to_2d(new_3d_position, intrinsic, extrinsic):
     return np.round(x).astype(np.int32), np.round(y).astype(np.int32)
 
 
-def register_image_with_depth(thecapture, depth_map, micasense_calib, basler_cameraMatrix):
+def register_image_with_depth(thecapture, depth_map, micasense_calib, basler_cameraMatrix, image_names):
     if thecapture.dls_present():
-        img_type = 'reflectance'
+        # reflectance
         irradiance_list = thecapture.dls_irradiance() + [0]
-        thecapture.plot_undistorted_reflectance(thecapture.dls_irradiance())
     else:
-        img_type = "radiance"
-        thecapture.plot_undistorted_radiance()
+        # radiance
         irradiance_list = None
+
     height, width = depth_map.shape
     images = thecapture.undistorted_reflectance(irradiance_list)
     registered_bands = []
+
     for i, image in enumerate(images):
         registered_band = np.zeros((height, width))
-        if i == 3:
-            break
 
-        print(f"Register Image from Band {i + 1} ")
         band_data = utils.get_band_data(micasense_calib, i)
+
         rotation = band_data['rotation']
         R = np.array(rotation)
         translation = band_data['translation']
@@ -64,7 +58,6 @@ def register_image_with_depth(thecapture, depth_map, micasense_calib, basler_cam
         micasense_extrinsic[:3, 3] = t.flatten()
         micasense_extrinsic[3, 3] = 1
 
-        print("help")
         cam_positions = np.stack(np.meshgrid(np.arange(width), np.arange(height)), axis=-1)
         new_3d_position = pixel_to_3d(cam_positions, depth_map, basler_cameraMatrix)
 
@@ -76,113 +69,91 @@ def register_image_with_depth(thecapture, depth_map, micasense_calib, basler_cam
         within_image = np.logical_and(within_image_x, within_image_y)
         valid_new_x = new_x[within_image]
         valid_new_y = new_y[within_image]
+
         values = image[valid_new_y, valid_new_x]
 
         x_positions = cam_positions[..., 0][within_image]
         y_positions = cam_positions[..., 1][within_image]
-        registered_band[y_positions, x_positions] = values
-     #  for y in range(height):
-     #      print(f"y {y} finish {range(height)}")
-     #      for x in range(width):
-     #          print(f"x,y of depth: {x, y}")
-     #          X, Y, Z = pixel_to_3d(x, y, depth_map, basler_cameraMatrix)
-     #          print(f"x,y z : {X, Y, Z}")
-     #          new_x, new_y = project_3d_to_2d(X, Y, Z, micasense_intrinsic, micasense_extrinsic)
-     #          print(f" new_x, new_y : {new_x, new_y}")
 
-     #          if 0 <= new_x < image.shape[1] and 0 <= new_y < image.shape[0]:
-     #              registered_band[y, x] = image[new_y, new_x]
+        registered_band[y_positions, x_positions] = values
         registered_bands.append(registered_band)
-    return registered_bands
+
+    return RegisteredMicasense(registered_bands, image_names)
+
+
+def set_intrinsic(thecapture, micasense_calib):
+    for i, image in enumerate(thecapture.images):
+        band_data = utils.get_band_data(micasense_calib, i)
+
+        camera_matrix = band_data['cameraMatrix']
+        dist_coeffs = band_data['distCoeffs']
+
+        focal_length = camera_matrix[0][0] / image.focal_plane_resolution_px_per_mm[0]
+        principal_point = (camera_matrix[0][2] / image.focal_plane_resolution_px_per_mm[0],
+                           camera_matrix[1][2] / image.focal_plane_resolution_px_per_mm[0])
+        distortion_parameters = dist_coeffs[0]
+        distortion_parameters = [distortion_parameters[0], distortion_parameters[1], distortion_parameters[4],
+                                 distortion_parameters[2], distortion_parameters[3]]
+        image.focal_length = focal_length
+        image.principal_point = principal_point
+        image.distortion_parameters = distortion_parameters
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Registration of the micasense images with depth information.')
     parser.add_argument('depth_path', type=str, help='Path to the directory containing depth information.')
     parser.add_argument('micasense_path', type=str, help='Path to the directory containing Micasense images')
-    parser.add_argument('image_number', type=str, nargs='?', default=None,
+    parser.add_argument('--output', type=str, nargs='?', default="output/depth_matching", help='Path to save images')
+    parser.add_argument('--image_number', type=str, nargs='?', default=None,
                         help='Image number based on the basler numbers with leading zeros (e.g., 000001)')
+    parser.add_argument('--basler_size', type=tuple, nargs='?', default=(5328, 4608),
+                        help='Original Basler image size (width, height)')
 
     args = parser.parse_args()
 
     depth_path = Path(args.depth_path)
     micasense_path = Path(args.micasense_path)
     image_number = args.image_number
+    basler_size = args.basler_size
+    output = args.output
 
-    depth_map = np.load(depth_path.as_posix() + f"/{image_number}_rect.npy")
-    print(depth_map)
-    width = 5328
-    height = 4608
-    new_size = (width, height)  # OpenCV erwartet (Breite, Höhe) als Tuple
-    #depth_map_resized = cv2.resize(depth_map, (width, height))
-    depth_map_resized = cv2.resize(depth_map, new_size, interpolation=cv2.INTER_CUBIC)
-    # 4. Entferne Rauschen (optional)
-    # Wende den Gaußschen Weichzeichner an, um Rauschen zu reduzieren
-    sigma = 1  # Stärke des Filters, anpassen je nach Bedarf
-    depth_map_resized = cv2.GaussianBlur(depth_map_resized, (0, 0), sigma)
-    #depth_map_resized = cv2.bilateralFilter(depth_map_resized, d=9, sigmaColor=75, sigmaSpace=75)
+    if image_number is None:
+        micasense_image_number = "*"
+    else:
+        micasense_image_number = utils.get_micasense_number_from_basler_number(image_number)
 
-    result = int(image_number) - 1
-    num_leading_zeros = len(image_number) - len(image_number.lstrip('0'))
-    formatted_result = f"{result:0{len(image_number)}d}"
-    micasense_image_number = formatted_result[2:]
-    image_names = list(micasense_path.glob(f'IMG_{micasense_image_number}_*.tif'))
+    image_names = sorted(list(micasense_path.glob(f'IMG_{micasense_image_number}_*.tif')))
     image_names = [x.as_posix() for x in image_names]
 
-    thecapture = capture.Capture.from_filelist(image_names)
-    micasense_calib = utils.read_micasense_calib("src/micasense_calib.yaml")
-    cal_samson_1 = utils.read_basler_calib(
-        "/media/david/T7/multispektral/20240416_calib/SAMSON1/SAMSON1_SAMSON2_stereo.yaml")
+    if len(image_names) < 6:
+        print(f"Warning: Expected at least 6 images, but found {len(image_names)}. Please check the image files.")
+        sys.exit()
+
+    micasense_calib = utils.read_micasense_calib("calib/micasense_calib.yaml")
+    cal_samson_1 = utils.read_basler_calib("calib/SAMSON1_SAMSON2_stereo.yaml")
     K_L, D_L, P_L, _ = cal_samson_1
-    for i, image in enumerate(thecapture.images):
-        print(f"Processing Band {i + 1} and setting calibrated parameters from micasense_calib.yaml")
 
-        band_data = utils.get_band_data(micasense_calib, i)
-        camera_matrix = band_data['cameraMatrix']
-        dist_coeffs = band_data['distCoeffs']
-        focal_length = camera_matrix[0][0] / image.focal_plane_resolution_px_per_mm[0]
-        principal_point = (camera_matrix[0][2]/image.focal_plane_resolution_px_per_mm[0], camera_matrix[1][2]/image.focal_plane_resolution_px_per_mm[0])
-        distortion_parameters = dist_coeffs[0]
-        distortion_parameters = [distortion_parameters[0],distortion_parameters[1],distortion_parameters[4],distortion_parameters[2],distortion_parameters[3]]
-        image.focal_length = focal_length
-        image.principal_point = principal_point
-        image.distortion_parameters = distortion_parameters
-    registered_band = register_image_with_depth(thecapture, depth_map_resized, micasense_calib, P_L)
-    print(len(registered_band))
+    for i in range(0, len(image_names), 6):
+        batch = image_names[i:i + 6]
+        depth_map_number = int(utils.get_number_from_image_name(batch[0])) + 1
+        file_to_depth_map = utils.find_depth_map_file(depth_path, depth_map_number)
 
-    band_names_lower = thecapture.band_names_lower()
-    rgb_band_indices = [band_names_lower.index('red'),
-                        band_names_lower.index('green'),
-                        band_names_lower.index('blue')]
+        if file_to_depth_map is None:
+            print(f"No depth map found for the micasense images {depth_map_number - 1}")
+            continue
 
-    first_band_data = registered_band[0]
-    height, width = first_band_data.shape
+        depth_map = np.load(file_to_depth_map)
+        depth_map_resized = cv2.resize(depth_map, basler_size, interpolation=cv2.INTER_LINEAR)
 
-    im_display = np.zeros((height, width, 3), dtype=np.float32)
+        thecapture = capture.Capture.from_filelist(batch)
 
-    for i, registered_image in enumerate(registered_band):
-        band_data = registered_image
-        im_min = np.percentile(band_data.flatten(), 0.5)
-        im_max = np.percentile(band_data.flatten(), 99.5)
+        set_intrinsic(thecapture, micasense_calib)
 
-        im_display[:, :, i] = (band_data - im_min) / (im_max - im_min) if im_max > im_min else 0
+        file_names = utils.extract_all_image_names(batch)
 
-    rgb = np.clip(im_display, 0, 1)
+        registered_bands = register_image_with_depth(thecapture, depth_map_resized, micasense_calib, P_L, file_names)
+        registered_bands.save_images(output)
 
-    plt.figure(figsize=(16, 13))
-    plt.imshow(rgb)
-    plt.title("RGB Composite from Original Images")
-    plt.axis('off')
-    output_png_path = "output/rgb_composite_depth.png"
-
-    plt.imsave(output_png_path, rgb, format='png')
-    print(f"Saved RGB composite as {output_png_path}.")
-
-    plt.show()
-    registered_band = np.array(registered_band)
-    registered_band = registered_band.transpose(2, 1, 0)
-    print(registered_band.shape)
-    thecapture._Capture__aligned_capture = registered_band
-    thecapture._Capture__aligned_radiometric_pan_sharpened_capture = [None, registered_band]
-    thecapture.save_capture_as_stack("test.tif", sort_by_wavelength=False, pansharpen=False)
-    print(thecapture.band_names_lower())
+        registered_bands = None
+        depth_map_resized = None
+        thecapture = None
