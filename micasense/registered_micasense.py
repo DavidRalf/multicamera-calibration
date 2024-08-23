269859f70efda138e22d306131b82077e25a38cf
David <David.Ralf@haw-hamburg.de>
2024-08-23 06:14:07 +0200

    some changes and static method for cropping the images

--- micasense/registered_micasense.py
+++ micasense/registered_micasense.py
@@ -4,10 +4,44 @@ import cv2
 import imageio
 import numpy as np
 from osgeo import gdal
-from osgeo_utils.gdal2tiles import filename
 
 import src.utils as utils
 from micasense import imageutils
+
+
+def crop(image_stack):
+    non_black_mask = np.all(image_stack > 0, axis=-1)
+
+    valid_coords = np.argwhere(non_black_mask)
+    top_left = valid_coords.min(axis=0)
+    bottom_right = valid_coords.max(axis=0) + 1
+
+    cropped_image = image_stack[top_left[0]:bottom_right[0], top_left[1]:bottom_right[1]]
+
+    height, width, _ = cropped_image.shape
+
+    row_threshold = int(0.9 * width)
+    col_threshold = int(0.9 * height)
+
+    non_zero_counts_rows = np.sum(np.all(cropped_image > 0, axis=-1), axis=1)
+    non_zero_counts_cols = np.sum(np.all(cropped_image > 0, axis=-1), axis=0)
+
+    top_left_row = np.argmax(non_zero_counts_rows >= row_threshold)
+    top_left_col = np.argmax(non_zero_counts_cols >= col_threshold)
+    top_left = (top_left_row, top_left_col)
+
+    bottom_right_row = height - np.argmax(
+        non_zero_counts_rows[::-1] >= row_threshold)
+    bottom_right_col = width - np.argmax(
+        non_zero_counts_cols[::-1] >= col_threshold)
+    bottom_right = (bottom_right_row, bottom_right_col)
+
+    if top_left is not None and bottom_right is not None:
+        final_cropped_image = cropped_image[top_left[0]:bottom_right[0], top_left[1]:bottom_right[1]]
+        return final_cropped_image
+    else:
+        print("Error in cropping return original stack")
+        return image_stack
 
 
 class RegisteredMicasense:
@@ -21,12 +55,16 @@ class RegisteredMicasense:
     }
 
     def __init__(self, images, file_names=None):
+        self.rgb_composite_crop = None
+        self.rgb_composite_enhanced_crop = None
         self.images = []
         self.load_images(images)
         self.file_names = file_names if file_names is not None else []
 
-        self.rgb_composite_enhanced = None
         self.rgb_composite = None
+        self.rgb_composite_crop = None
+        self.rgb_composite_enhanced = None
+        self.rgb_composite_enhanced_crop = None
 
     def __repr__(self):
         return f'RegisteredMicasense with {len(self.images)} images.'
@@ -93,7 +131,6 @@ class RegisteredMicasense:
         raise ValueError(f"Band name '{band_name}' is not recognized.")
 
     def save_images(self, output_directory):
-        """Saves each image to the specified directory with the corresponding file name."""
         if not os.path.exists(output_directory):
             os.makedirs(output_directory)
 
@@ -117,7 +154,7 @@ class RegisteredMicasense:
     def get_rgb_indices(self):
         return [2, 1, 0]
 
-    def make_rgb_composite(self):
+    def make_rgb_composite(self, crop_image):
         rgb_band_indices = self.get_rgb_indices()
 
         band_red = self.get_image(rgb_band_indices[0])
@@ -133,39 +170,19 @@ class RegisteredMicasense:
 
         for i, band_index in enumerate(rgb_band_indices):
             im_display[:, :, i] = imageutils.normalize(rgb_stack[:, :, i], im_min, im_max)
-        self.rgb_composite = im_display
-
-    def save_rgb_composite(self, output_directory="../output/comp"):
-        if not os.path.exists(output_directory):
-            os.makedirs(output_directory)
-        filename=os.path.join(output_directory, f"rgb_composite_{utils.get_number_from_image_name(self.file_names[0])}.png")
-        imageio.imwrite(filename, (255 * self.get_rgb_composite()).astype('uint8'))
-
-    def get_rgb_composite(self):
-        if self.rgb_composite is not None:
-            return self.rgb_composite
+        if crop_image:
+            self.rgb_composite_crop = crop(im_display)
         else:
-            self.make_rgb_composite()
-            return self.rgb_composite
+            self.rgb_composite = im_display
 
-    def save_rgb_composite_enhanced(self, output_directory="../output/comp"):
-        if not os.path.exists(output_directory):
-            os.makedirs(output_directory)
-        filename = os.path.join(output_directory,f"rgb_composite_enhanced_{utils.get_number_from_image_name(self.file_names[0])}.png")
-        imageio.imwrite(filename, (255 * self.get_rgb_composite_enhanced()).astype('uint8'))
+    def make_rgb_enhancement(self, crop):
+        if crop and self.rgb_composite_crop is None:
+            self.make_rgb_composite(crop)
+        elif not crop and self.rgb_composite is None:
+            self.make_rgb_composite(crop)
 
-    def get_rgb_composite_enhanced(self):
-        if self.rgb_composite_enhanced is not None:
-            return self.rgb_composite_enhanced
-        else:
-            self.make_rgb_enhancement()
-            return self.rgb_composite_enhanced
+        rgb_composite = self.rgb_composite_crop if crop else self.rgb_composite
 
-    def make_rgb_enhancement(self):
-        if self.rgb_composite is None:
-            self.make_rgb_composite()
-
-        rgb_composite = np.copy(self.get_rgb_composite())
         gaussian_rgb = cv2.GaussianBlur(rgb_composite, (9, 9), 10.0)
         gaussian_rgb[gaussian_rgb < 0] = 0
         gaussian_rgb[gaussian_rgb > 1] = 1
@@ -175,4 +192,45 @@ class RegisteredMicasense:
         # Apply a gamma correction to make the render appear closer to what our eyes would see
         gamma = 1.4
         gamma_corr_rgb = unsharp_rgb ** (1.0 / gamma)
-        self.rgb_composite_enhanced = gamma_corr_rgb
+        if crop:
+            self.rgb_composite_enhanced_crop = gamma_corr_rgb
+        else:
+            self.rgb_composite_enhanced = gamma_corr_rgb
+
+    def save_rgb_composite(self, crop=False, enhanced=False, output_directory="../output/comp"):
+        if not os.path.exists(output_directory):
+            os.makedirs(output_directory)
+
+            # Build the filename based on crop and enhancement flags
+        file_name = os.path.join(
+            output_directory,
+            f"rgb_composite{'_enhanced' if enhanced else ''}{'_crop' if crop else ''}_{utils.get_number_from_image_name(self.file_names[0])}.png"
+        )
+
+        imageio.imwrite(file_name, (255 * self.get_rgb_composite(crop, enhanced)).astype('uint8'))
+
+    def get_rgb_composite(self, crop=False, enhanced=False):
+        if crop:
+            return self._get_rgb_composite_crop() if not enhanced else self._get_rgb_composite_enhanced_crop()
+        else:
+            return self._get_rgb_composite() if not enhanced else self._get_rgb_composite_enhanced()
+
+    def _get_rgb_composite(self):
+        if self.rgb_composite is None:
+            self.make_rgb_composite(False)
+        return self.rgb_composite
+
+    def _get_rgb_composite_crop(self):
+        if self.rgb_composite_crop is None:
+            self.make_rgb_composite(True)
+        return self.rgb_composite_crop
+
+    def _get_rgb_composite_enhanced(self):
+        if self.rgb_composite_enhanced is None:
+            self.make_rgb_enhancement(False)
+        return self.rgb_composite_enhanced
+
+    def _get_rgb_composite_enhanced_crop(self):
+        if self.rgb_composite_enhanced_crop is None:
+            self.make_rgb_enhancement(True)
+        return self.rgb_composite_enhanced_crop
