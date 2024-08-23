1628c20c8d8e32c725ed092115a876ef62ddf801
David <David.Ralf@haw-hamburg.de>
2024-08-23 06:14:42 +0200

    tested the changes

--- src/playground.py
+++ src/playground.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 
 from registration.registration_with_depth_matching import register, set_intrinsic
+from registration.registration_with_feature_matching import register as register_feature
 import utils as utils
 from pathlib import Path
 from micasense import capture
@@ -35,7 +36,56 @@ set_intrinsic(thecapture, micasense_calib)
 file_names = utils.extract_all_image_names(image_names)
 
 registered_bands = register(thecapture, depth_map_resized, micasense_calib, P_L, file_names)
+#registered_bands = register_feature(thecapture, image_names,"stack",True,file_names)
 
 composite = registered_bands.get_rgb_composite()
-registered_bands.save_rgb_composite()
-registered_bands.save_rgb_composite_enhanced()
+#registered_bands.save_rgb_composite()
+#registered_bands.save_rgb_composite_enhanced()
+
+# Draw the bounding box on the image
+bgr_image = cv2.cvtColor(composite, cv2.COLOR_RGB2BGR)
+# Display the image with the bounding box
+cv2.namedWindow('Image composite basic', cv2.WINDOW_NORMAL)
+cv2.imshow('Image composite basic', bgr_image)
+cv2.resizeWindow('Image composite basic', 800, 600)  # Set the window size (adjust as needed)
+
+
+
+
+
+# Draw the bounding box on the image
+composite_crop = registered_bands.get_rgb_composite(crop=True)
+bgr_image = cv2.cvtColor(composite_crop, cv2.COLOR_RGB2BGR)
+# Display the image with the bounding box
+cv2.namedWindow('Image composite crop', cv2.WINDOW_NORMAL)
+cv2.imshow('Image composite crop', bgr_image)
+cv2.resizeWindow('Image composite crop', 800, 600)  # Set the window size (adjust as needed)
+
+
+
+
+
+
+# Draw the bounding box on the image
+composite_enhanced = registered_bands.get_rgb_composite(enhanced=True)
+bgr_image = cv2.cvtColor(composite_enhanced, cv2.COLOR_RGB2BGR)
+# Display the image with the bounding box
+cv2.namedWindow('Image composite enhanced', cv2.WINDOW_NORMAL)
+cv2.imshow('Image composite enhanced', bgr_image)
+cv2.resizeWindow('Image composite enhanced', 800, 600)  # Set the window size (adjust as needed)
+
+
+
+
+# Draw the bounding box on the image
+composite_enhanced_crop = registered_bands.get_rgb_composite(enhanced=True,crop=True)
+bgr_image = cv2.cvtColor(composite_enhanced_crop, cv2.COLOR_RGB2BGR)
+# Display the image with the bounding box
+cv2.namedWindow('Image composite enhanced_crop', cv2.WINDOW_NORMAL)
+cv2.imshow('Image composite enhanced_crop', bgr_image)
+cv2.resizeWindow('Image composite enhanced_crop', 800, 600)  # Set the window size (adjust as needed)
+
+
+
+
+cv2.waitKey(0)
